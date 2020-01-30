910c729f9f0c5da20a6ea3e3df3a564b01410831
Connor Jones <ConnorLeeJones@gmail.com>
2020-01-30 00:49:20 -0500

    pagination progress

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-pagination',
@@ -6,15 +7,43 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChange
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent implements OnInit {
-  title = 'custom-pagination-angular';  
-  data = [];  
-  pagedItems: Array<any>;  
-  constructor() { }  
+  page: number = 1;
+  userId: number = 2;
+  ratings: Array<any>;  
+  pages: Array<number>;
+  totalItems: number;
+  itemsPerPage: number;
+
+  constructor(private userService: UserService) { }  
+
   ngOnInit() {  
-    this.data = Array(1000).fill(0).map((x, i) => ({ id: (i + 1), name: `Item Paged ${i + 1}`, product: `Product ${i + 1}` }));  
-  }  
-  beginPagination(pagedItems: Array<any>) {  
-    this.pagedItems = pagedItems;  
-  }  
+    // this.data = Array(1000).fill(0).map((x, i) => ({ id: (i + 1), name: `Item Paged ${i + 1}`, product: `Product ${i + 1}` }));  
+    this.getRatings();
+  
+  } 
+
+  getRatings(){
+    this.userService.getUserPaginatedArtists(this.userId, this.page - 1).subscribe(data =>
+      {console.log(data);
+        this.ratings = data['content'];
+        this.pages = new Array(data['totalPages']);
+        this.totalItems = data['totalElements'];
+        this.itemsPerPage = data['size'];
+      },
+      (error) => console.log(error.error.message)
+      );
+  }
+
+  loadPage($event: any){
+    console.log($event);
+    this.page = $event;
+    this.getRatings();
+  }
+
+
+
+  // beginPagination(pagedItems: Array<any>) {  
+  //   this.pagedItems = pagedItems;  
+  // }  
 
 }
